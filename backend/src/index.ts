898010f2ef87b96181eaedfd61520b0754ddc4e9
Mohamed Hamdy Mohamed <mohamed547754@gmail.com>
2025-05-01 18:20:10 +0300

    CORS Middleware In Backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import sequelize from './config/database';
 import apartmentRoutes from './routes/apartmentRoutes';
@@ -6,13 +7,22 @@ import apartmentRoutes from './routes/apartmentRoutes';
 dotenv.config();
 
 const app = express();
+
+// CORS middleware to solve
+// cors error when sending request from frontend
+app.use(cors({
+    origin: 'http://localhost:3000', // Frontend URL
+    credentials: true, // Optional: if you're using cookies/auth headers
+}));
+
 app.use(express.json());
 
 // Routes
 app.use('/api/apartments', apartmentRoutes);
+
 const PORT = process.env.PORT || 5000;
 
-sequelize.sync({ alter: true }) // or use { force: true } during development
+sequelize.sync({ alter: true })
     .then(() => {
         console.log('Database synced');
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
@@ -20,4 +30,3 @@ sequelize.sync({ alter: true }) // or use { force: true } during development
     .catch((err) => {
         console.error('Error syncing DB:', err);
     });
-
